76efdf0d696a9308085c8a15be93677282144460
EgorLiutov22 <93601126+EgorLiutov22@users.noreply.github.com>
2023-02-24 19:29:02 +0500

    ADD requirements.txt

--- nnet.py
+++ nnet.py
@@ -5,11 +5,18 @@ import numpy as np
 import cv2
 
 
+def preprocess_image(image):
+    image = np.array(image)
+
+    sample_image_resized = cv2.resize(image, (224, 224))
+
+    sample_image = np.expand_dims(sample_image_resized, axis=0)
+
+    return sample_image
+
+
 class Network:
     def __init__(self, image):
-        # self.__model = tf.keras.Sequential([
-        #     hub.KerasLayer("https://tfhub.dev/sayakpaul/distill_bit_r50x1_160_feature_extraction/1", trainable=True)
-        # ])
         self.__model = tf.keras.applications.ResNet50V2(
             include_top=True,
             weights="imagenet",
@@ -19,23 +26,7 @@ class Network:
             classes=1000,
             classifier_activation="softmax",
         )
-        self.__image = self.preprocess_image(image)
-
-    def preprocess_image(self, image):
-        image = np.array(image)
-        # # Resize to (160, 160)
-        # image_resized = tf.image.resize(image, (160, 160))
-        # img_reshaped = tf.reshape(image_resized, [1, image.shape[0], image.shape[1], image.shape[2]])
-        # # Use `convert_image_dtype` to convert to floats in the [0,1] range.
-        # image = tf.image.convert_image_dtype(img_reshaped, tf.float32)
-        # sample_image = cv2.imread(str(image))
-        #
-        sample_image_resized = cv2.resize(image, (224, 224))
-
-        sample_image = np.expand_dims(sample_image_resized, axis=0)
-
-        return sample_image
-
+        self.__image = preprocess_image(image)
 
     def prediction(self):
         predictions = self.__model.predict(self.__image)
